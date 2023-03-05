8458dda7cefdcead9e37effff70aa69aab921e7a
Luis Toro <ltoro@qvision.com.co>
2023-03-04 23:30:31 -0500

    Agregando common en rama develop

---
+++ servicio-commons/src/main/java/co/com/qvision/app/commons/ServicioCommonsApplication.java
@@ -0,0 +1,8 @@
+package co.com.qvision.app.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCommonsApplication {
+
+}
---
+++ servicio-commons/src/test/java/co/com/qvision/app/commons/ServicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.qvision.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
